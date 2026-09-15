355ba7b727ca0318a83e60eb327f89f2f75c1a76
vanishree00 <vanishreemalli2007@gmail.com>
2026-09-15 19:22:09 +0530

    minor changes

--- python-mastery/DSA/linkedList.py
+++ python-mastery/DSA/linkedList.py
@@ -8,17 +8,22 @@ class Node:
      while current!= None:
       print(current.data, end= "-->") 
       current = current.next
-     print("None")        
-        
+     print("None")       
+
+     def insert_at_begining(self, data):
+        newNode4 = Node(data)
+        newNode4.next = self
+        return newNode4
+
 newNode1=Node(10)
 newNode2=Node(20)
 newNode3=Node(30)
 newNode1.next = newNode2
 newNode2.next = newNode3
 head= newNode1
-newNode4= Node(40)   #inserting at begninng
-newNode4.next = head
-head = newNode4
+# newNode4= Node(40)   #inserting at begninng
+# newNode4.next = head
+# head = newNode4
 newNode5 = Node(50)   #inserting at end
 current = head 
 while current.next != None:
@@ -29,5 +34,6 @@ print(head.next.data)
 print(head.next.next.data)
 
 head.printLinkedList()   #calling the method directly on head node
-
+head =head.insert_at_begining(40)
+head.printLinkedList()
 
