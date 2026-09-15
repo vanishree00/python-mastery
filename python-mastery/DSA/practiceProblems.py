5307c0678f19cddd2e1dcf656eb30a5939a65f53
vanishree00 <vanishreemalli2007@gmail.com>
2026-09-15 20:12:36 +0530

    printing linked list and deleting the given node

--- python-mastery/DSA/practiceProblems.py
+++ python-mastery/DSA/practiceProblems.py
@@ -2,6 +2,13 @@ class Solution:
     def __init__(self, data, next=None):
         self.data=data
         self.next=next
+
+    def linkedList(self):
+        current = self
+        while current != None:
+            print(current.data, end="-->")
+            current= current.next  
+        print("None")         
     def middleNode(self):
         slow=self
         fast=self
@@ -9,8 +16,11 @@ class Solution:
             slow = slow.next
             fast = fast.next.next
         return slow    
-            
 
+    def deleteNode(self, node):
+        node.data = node.next.data
+        node.next = node.next.next
+        
 node1=Solution(1)
 node2= Solution(2)
 node3=Solution(3)
@@ -20,5 +30,10 @@ node1.next=node2
 node2.next=node3
 node3.next=node4
 node4.next=node5
+head=node1
+head.linkedList()
 middle = node1.middleNode()
 print(f" middle node is : {middle.data} ")
+node1.deleteNode(node3)
+print("After deleting the given node-")
+head.linkedList()
